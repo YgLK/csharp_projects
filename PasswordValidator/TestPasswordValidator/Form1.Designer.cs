5c7020111d98ad7bd04d1be63e4196811cdb02f0
YgLK <kuba21316@wp.pl>
2022-05-20 16:57:09 +0200

    Add final form of Password Validator control

---
+++ PasswordValidator/TestPasswordValidator/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TestPasswordValidator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordValidator1 = new PasswordValidatorControl.PasswordValidator();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(259, 117);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(227, 23);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter your password:";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(312, 359);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(97, 34);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(259, 53);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(227, 23);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter your username:";
+            // 
+            // passwordValidator1
+            // 
+            this.passwordValidator1.Location = new System.Drawing.Point(222, 163);
+            this.passwordValidator1.MinCapLetterCountProp = 3;
+            this.passwordValidator1.MinCharCountProp = 8;
+            this.passwordValidator1.MinDigitCountProp = 2;
+            this.passwordValidator1.MinSpecCharCountProp = 4;
+            this.passwordValidator1.Name = "passwordValidator1";
+            this.passwordValidator1.Size = new System.Drawing.Size(293, 190);
+            this.passwordValidator1.TabIndex = 7;
+            this.passwordValidator1.TxtBoxPasswordProp = this.txtPassword;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.passwordValidator1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private TextBox txtPassword;
+        private Label label1;
+        private Button btnRegister;
+        private TextBox txtUsername;
+        private Label label2;
+        private PasswordValidatorControl.PasswordValidator passwordValidator1;
+    }
+}
